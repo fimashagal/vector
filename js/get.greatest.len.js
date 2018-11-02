3beb334526978e0ff22769009864f7bfd4308c7d
fimashagal <fimashagal@gmail.com>
2018-11-02 23:12:46 +0200

    init

--- js/get.greatest.len.js
+++ js/get.greatest.len.js
@@ -3,7 +3,9 @@ function getGreatestLen(arrays){
     let response = 0;
     for(let array of arrays) {
         let diff = Math.floor(array[array.length - 1] - array[0]);
-        if(diff > response) response = diff;
+        if(diff > response) {
+            response = diff;
+        }
     }
     return response;
 }
